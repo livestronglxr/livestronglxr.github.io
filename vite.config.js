c7008fea983cc8d45d2f8ac812d3b3fa961845ad
root <root@localhost.localdomain>
2026-03-15 22:58:44 +0800

    fix: 将dist内容移到根目录，删除源码

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',  // 使用相对路径，适用于 GitHub Pages
-})
