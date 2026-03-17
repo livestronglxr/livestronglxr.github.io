e1dda914a19bb6e486808b39d995266c3b9098bd
root <root@localhost.localdomain>
2026-03-17 09:11:34 +0800

    fix: 更新 base 路径为 livestronglxr.github.io

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lixiangrui.github.io/',  // 项目页面路径
+  base: '/livestronglxr.github.io/',  // 项目页面路径
 })
